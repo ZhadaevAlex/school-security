c8d180f0d36a63211a4f28f0e5b51dd3feccb877
Alex Zhadaev <zhadaevs@yandex.ru>
2023-01-10 11:34:28 +0300

    Initial commit

---
+++ src/test/java/ru/zhadaev/schoolsecurity/SchoolSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.zhadaev.schoolsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
